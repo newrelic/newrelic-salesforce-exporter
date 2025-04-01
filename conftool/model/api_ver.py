4cfb0172e02f4ebdc5c411536320bcb8eeb8b1c4
Andreu <andreu.santaren@gmail.com>
2025-04-01 15:23:22 +0200

    add: validation checks for redis and arguments models.

--- conftool/model/api_ver.py
+++ conftool/model/api_ver.py
@@ -4,8 +4,6 @@ class ApiVer(BaseModel):
     __inner_val__: str
 
     def check(self):
-        if self.__inner_val__ == "":
-            return
         components: list[str] = self.__inner_val__.split(".")
         if len(components) != 2:
             raise Exception(f"Wrong api_ver value `{self.__inner_val__}`, it must have the format 'X.Y'")
--- conftool/model/instance.py
+++ conftool/model/instance.py
@@ -4,6 +4,8 @@ from .service_schedule import ServiceScheduleModel
 from .query import QueryModel
 from .api_ver import ApiVer
 
+import validators
+
 class LimitsModel(BaseModel):
     api_ver: ApiVer
     names: list[str]
@@ -12,12 +14,17 @@ class GenerationIntervalModel(Enum):
     HOURLY = 'Hourly'
     DAILY = 'Daily'
 
+#TODO: add attributes for the JWT flow
 class AuthModel(BaseModel):
     grant_type: str
     client_id: str
     client_secret: str
     username: str
     password: str
+
+    def check(self):
+        #TODO: check
+        pass
 
 class RedisModel(BaseModel):
     host: str
@@ -26,6 +33,18 @@ class RedisModel(BaseModel):
     password: str
     ssl: bool
     expire_days: int
+
+    def check(self):
+        if validators.domain(self.host) != True and \
+           validators.ipv4(self.host) != True and \
+           validators.ipv6(self.host) != True:
+            raise Exception(f"`host` must be a valid domain name or IP address")
+        if self.port < 0 or self.port > 65535:
+            raise Exception(f"`port` is `{self.port}` and must be a valid TCP port value [0, 65535]")
+        if self.db_number < 0 or self.db_number > 15:
+            raise Exception(f"`db_number` must be a valid Redis DB number [0, 15]")
+        if self.expire_days < 0 != True:
+            raise Exception(f"`expire_days` can't be negative")
 
 class ArgumentsModel(BaseModel):
     auth: AuthModel
@@ -42,8 +61,10 @@ class ArgumentsModel(BaseModel):
     limits: LimitsModel
 
     def check(self):
-        #TODO: check
-        pass
+        if validators.url(self.token_url) != True:
+            raise Exception(f"Wrong URL format in `token_url`")
+        if self.time_lag_minutes < 0:
+            raise Exception(f"`time_lag_minutes`, can't be negative")
 
 class InstanceModel(BaseModel):
     name: str
