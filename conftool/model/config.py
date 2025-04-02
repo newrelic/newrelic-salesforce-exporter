08cf1fdab52c03a56c912a4351516ecdbf982af1
Andreu <andreu.santaren@gmail.com>
2025-04-02 13:35:42 +0200

    add: `newrelic` config key.

--- conftool/model/config.py
+++ conftool/model/config.py
@@ -2,6 +2,7 @@ from .base import BaseModel
 from .service_schedule import ServiceScheduleModel
 from .instance import InstanceModel
 from .query import QueryModel
+from .newrelic import NewRelicModel
 from .exception import ConfigException
 
 class ConfigModel(BaseModel):
@@ -11,22 +12,20 @@ class ConfigModel(BaseModel):
     service_schedule: ServiceScheduleModel
     instances: list[InstanceModel]
     queries: list[QueryModel]
+    newrelic: NewRelicModel
 
     def check(self):
-        if self.integration_name is None:
-            raise ConfigException("integration_name must be defined")
-        if self.integration_name == "":
-            raise ConfigException("integration_name can't be empty")
         if self.run_as_service:
-            if self.cron_interval_minutes is None:
-                raise ConfigException("cron_interval_minutes must be defined")
-            if self.cron_interval_minutes <= 0:
-                raise ConfigException("cron_interval_minutes must be greater than 0")
+            if self.cron_interval_minutes is not None:
+                if self.cron_interval_minutes <= 0:
+                    raise ConfigException("`cron_interval_minutes` must be greater than 0")
         else:
             if self.service_schedule is None:
-                raise ConfigException("service_schedule must be set")
+                raise ConfigException("`service_schedule` must be defined")
         if self.instances is None:
-            raise ConfigException("instances must be defined")
+            raise ConfigException("`instances` must be defined")
         if len(self.instances) == 0:
-            raise ConfigException("instances must contain at least one entry")
+            raise ConfigException("`instances` must contain at least one entry")
+        if self.newrelic is None:
+            raise ConfigException("`newrelic` must be defined")
         super().check()
---
+++ conftool/model/newrelic.py
@@ -0,0 +1,29 @@
+from .base import BaseModel
+from enum import Enum
+from .exception import ConfigException
+
+class DataFormatModel(Enum):
+   EVENTS = "events"
+   LOGS = "logs"
+
+class ApiEndpointModel(Enum):
+   US = "US"
+   EU = "EU"
+   FEDRAMP = "FEDRAMP"
+   
+class NewRelicModel(BaseModel):
+    data_format: DataFormatModel
+    api_endpoint: ApiEndpointModel
+    account_id: str
+    license_key: str
+
+    def check(self):
+        if self.data_format is None:
+           raise ConfigException("`data_format` must be defined")
+        if self.api_endpoint is None:
+           raise ConfigException("`api_endpoint` must be defined")
+        if self.data_format is DataFormatModel.EVENTS and self.account_id is None:
+           raise ConfigException("`account_id` must be defined")
+        if self.license_key is None:
+            raise ConfigException("`license_ley` must be defined")
+        return super().check()
