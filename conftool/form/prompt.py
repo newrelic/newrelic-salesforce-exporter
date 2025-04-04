c4e3112a8edf8fb61237630dd2d5ebd54047dc6a
Andreu <andreu.santaren@gmail.com>
2025-04-04 11:53:28 +0200

    add: prompts for int and bool.

--- conftool/form/prompt.py
+++ conftool/form/prompt.py
@@ -1,14 +1,62 @@
 from prompt_toolkit.validation import Validator, ValidationError
 from prompt_toolkit import prompt
 
-def prompt_enum(message: str, options: list[str], required: bool) -> str:
-    # Print list
+def prompt_list(message: str, options: list[str], required: bool) -> int:
     for i,option in enumerate(options):
         print(str(i+1) + ") " + str(option))
     validator = ListNumberValidator(1, len(options), not required)
-    return prompt(message + " ", validator=validator)
+    response = prompt(message + " ", validator=validator)
+    if response is None:
+        return None
+    else:
+        return int(response)
+    
+def prompt_int(message: str, min: int, max: int, required: bool) -> int:
+    validator = NumberRangeValidator(min, max, not required)
+    response = prompt(message + " ", validator=validator)
+    if response is None or response == "":
+        return None
+    else:
+        return int(response)
+    
+def prompt_bool(message: str, required: bool) -> int:
+    validator = BooleanValidator(not required)
+    response = prompt(message + " ", validator=validator)
+    if response is None or response == "":
+        return None
+    else:
+        return response == "y" or response == "Y"
+
     
 class ListNumberValidator(Validator):
+    min: int
+    max: int
+    skippable: bool
+
+    def __init__(self, min: int, max: int, skippable: bool):
+        self.min = min
+        self.max = max
+        self.skippable = skippable
+        super().__init__()
+
+    def validate(self, document):
+        text = document.text
+        if not self.digit_in_range(text):
+            if self.skippable and text == "":
+                return
+            raise ValidationError(
+                message="Input must be a number in the list",
+                cursor_position=0
+            )
+    
+    def digit_in_range(self, text: str) -> bool:
+        if text.isdigit():
+            n = int(text)
+            return n >= self.min and n <= self.max
+        else:
+            return False
+        
+class NumberRangeValidator(Validator):
     min: int
     max: int
     skippable: bool
@@ -25,13 +73,30 @@ class ListNumberValidator(Validator):
             if self.skippable and text == "":
                 return
             raise ValidationError(
-                message="Input must be a number in the list",
+                message=f"Input must be a number in the range [{self.min},{self.max}]",
                 cursor_position=0
             )
     
     def number_in_range(self, text: str) -> bool:
-        if text.isdigit():
+        if text.isdecimal():
             n = int(text)
             return n >= self.min and n <= self.max
         else:
             return False
+        
+class BooleanValidator(Validator):
+    skippable: bool
+
+    def __init__(self, skippable: bool):
+        self.skippable = skippable
+        super().__init__()
+
+    def validate(self, document):
+        text = document.text
+        if text not in {"y", "Y", "n", "N"}:
+            if self.skippable and text == "":
+                return
+            raise ValidationError(
+                message=f"Input must be a 'Y', 'y', 'N' or 'n'",
+                cursor_position=0
+            )
--- conftool/form/questionnaire.py
+++ conftool/form/questionnaire.py
@@ -1,43 +1,63 @@
 from dataclasses import dataclass
 from enum import Enum
 from conftool.model.api_endpoint import ApiEndpointModel
-from .prompt import prompt_enum
+from .prompt import prompt_list, prompt_int, prompt_bool
 
 @dataclass
 class Question:
     description: str
     required: bool
     prompt: str
-    datatype: type
+    datatype: type = None
 
 def run():
-    r = ask(Question(
+    ask_enum(Question(
         description="New Relic API endpoint",
         required=True,
-        prompt="API Endpoint?",
+        prompt="API Endpoint (1-3)?",
         datatype=ApiEndpointModel))
-    print_response(r)
+    ask_int(Question(
+        description="Redis server port number",
+        required=False,
+        prompt="Port (0-65535)?"),
+        0, 65535)
+    ask_bool(Question(
+        description="Enable cache for event deduplication",
+        required=False,
+        prompt="Cache enabled (y/n)?"))
+
 
 #TODO: function to check constrains (URL format, cron format, etc)
 #TODO: custom error message when checker fails
 
-def ask(question: Question) -> any:
-    print_question(question)
-    if issubclass(question.datatype, Enum):
-        return ask_enum(question)
-    #TODO: support all possible data types
+# Ask for user responses
 
-def ask_enum(question: Question) -> any:
+def ask_enum(question: Question) -> Enum:
+    print_question(question)
     options = [e.value for e in question.datatype]
-    response = prompt_enum(question.prompt, options, question.required)
-    if response == "":
-        return None
+    num = prompt_list(question.prompt, options, question.required)
+    if num is None:
+        r = None
     else:
-        number = int(response)
-    return question.datatype(options[number - 1])
+        r = question.datatype(options[num - 1])
+    print_response(r)
+    return r
+
+def ask_int(question: Question, min: int, max: int) -> int:
+    print_question(question)
+    r = prompt_int(question.prompt, min, max, question.required)
+    print_response(r)
+    return r
+
+def ask_bool(question: Question) -> bool:
+    print_question(question)
+    r = prompt_bool(question.prompt, question.required)
+    print_response(r)
+
+# Print formating
 
 def print_response(r):
     print("-> Response =", r)
 
-def print_question(question):
+def print_question(question: Question):
     print(question.description + " (" + ("required" if question.required else "optional") + ")")
