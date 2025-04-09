9c73b327d1a8d76e4efe3fd6676a00851ffdacdb
Andreu <andreu.santaren@gmail.com>
2025-04-09 13:58:26 +0200

    add: text module.

--- conftool/form/question.py
+++ conftool/form/question.py
@@ -1,18 +1,18 @@
 from dataclasses import dataclass
 from enum import Enum
 from .prompt import prompt_list, prompt_int, prompt_bool, prompt_str, prompt_any
+from .text import Text
 
 @dataclass
 class Question:
-    description: str
+    text: Text
     required: bool
-    prompt: str
     datatype: type = None
 
 def ask_enum(question: Question) -> Enum:
     print_question(question)
     options = [e.value for e in question.datatype]
-    num = prompt_list(question.prompt, options, question.required)
+    num = prompt_list(question.text.prom, options, question.required)
     if num is None:
         r = None
     else:
@@ -22,25 +22,25 @@ def ask_enum(question: Question) -> Enum:
 
 def ask_int(question: Question, min: int, max: int) -> int:
     print_question(question)
-    r = prompt_int(question.prompt, min, max, question.required)
+    r = prompt_int(question.text.prom, min, max, question.required)
     print_response(r)
     return r
 
 def ask_bool(question: Question) -> bool:
     print_question(question)
-    r = prompt_bool(question.prompt, question.required)
+    r = prompt_bool(question.text.prom, question.required)
     print_response(r)
     return r
 
 def ask_str(question: Question, checker) -> str:
     print_question(question)
-    r = prompt_str(question.prompt, checker, question.required)
+    r = prompt_str(question.text.prom, checker, question.required)
     print_response(r)
     return r
 
 def ask_any(question: Question) -> str:
     print_question(question)
-    r = prompt_any(question.prompt, question.required)
+    r = prompt_any(question.text.prom, question.required)
     print_response(r)
     return r
 
@@ -48,6 +48,9 @@ def ask_any(question: Question) -> str:
 
 def print_response(r):
     print("-> Response =", r)
+    print()
 
 def print_question(question: Question):
-    print(question.description + " (" + ("required" if question.required else "optional") + ")")
+    print(question.text.desc)
+    print("Required." if question.required else "Optional.")
+    print()
--- conftool/form/questionnaire.py
+++ conftool/form/questionnaire.py
@@ -1,60 +1,87 @@
-from conftool.model.api_endpoint import ApiEndpointModel
+from conftool.model.config import ConfigModel
+from conftool.model.exception import ConfigException
+from conftool.model.instance import InstanceModel
+from conftool.model.service_schedule import ServiceScheduleModel
 from .question import Question, ask_int, ask_enum, ask_bool, ask_str, ask_any
+from .text import *
 
 def run():
-    print("REQUIRED")
+    conf = ConfigModel()
 
-    ask_enum(Question(
-        description="New Relic API endpoint",
-        required=True,
-        prompt="API Endpoint (1-3)?",
-        datatype=ApiEndpointModel))
-    ask_int(Question(
-        description="Redis server port number",
-        required=True,
-        prompt="Port (0-65535)?"),
-        0, 65535)
-    ask_bool(Question(
-        description="Enable cache for event deduplication",
-        required=True,
-        prompt="Cache enabled (y/n)?"))
-    ask_str(Question(
-        description="A whole word with no spaces",
-        required=True,
-        prompt="Word?"),
-        word_check)
+    # Integration name
+    conf.integration_name = \
     ask_any(Question(
-        description="Just put anything here",
-        required=True,
-        prompt="String?"))
-    
-    print("OPTIONAL")
+        text=t_integration_name,
+        required=False))
 
-    ask_enum(Question(
-        description="New Relic API endpoint",
-        required=False,
-        prompt="API Endpoint (1-3)?",
-        datatype=ApiEndpointModel))
-    ask_int(Question(
-        description="Redis server port number",
-        required=False,
-        prompt="Port (0-65535)?"),
-        0, 65535)
+    # Run as service
+    conf.run_as_service = \
     ask_bool(Question(
-        description="Enable cache for event deduplication",
-        required=False,
-        prompt="Cache enabled (y/n)?"))
-    ask_str(Question(
-        description="A whole word with no spaces",
-        required=False,
-        prompt="Word?"),
-        word_check)
-    ask_any(Question(
-        description="Just put anything here",
-        required=False,
-        prompt="String?"))
+        text=t_run_as_service,
+        required=False))
 
-#TODO: custom error message when checker fails
+    if conf.run_as_service is None or conf.run_as_service == False:
+        conf.cron_interval_minutes = \
+        ask_int(Question(
+            text=t_cron_interval,
+            required=False),
+            1, 10000)
+    else:
+        do_setup_service_schedule = \
+        ask_bool(Question(
+            text=t_conf_scheduler,
+            required=True))
 
-def word_check(text: str) -> bool:
-    return not (' ' in text)
+        if do_setup_service_schedule:
+            service_schedule = ServiceScheduleModel()
+            service_schedule.hour = \
+            ask_str(Question(
+                text=t_service_scheduler_hours,
+                required=True),
+                cron_config_check_hours)
+            service_schedule.minute = \
+            ask_str(Question(
+                text=t_service_scheduler_mins,
+                required=True),
+                cron_config_check_minutes)
+            conf.service_schedule = service_schedule
+
+    # Instances
+    conf.instances = list()
+    num_instances = \
+    ask_int(Question(
+        text=t_num_instances,
+        required=True),
+        1, 10)
+
+    for _ in range(num_instances):
+        i = instance_questions()
+        conf.instances.append(i)
+
+    print("Final config model:\n")
+    print(conf.to_yaml())
+
+    # ask_enum(Question(
+    #     text="New Relic API endpoint",
+    #     required=False,
+    #     prompt="API Endpoint (1-3)?",
+    #     datatype=ApiEndpointModel))
+
+def instance_questions() -> InstanceModel:
+    pass
+
+# Format checkers
+
+def cron_config_check_hours(text: str) -> bool:
+    try:
+        ServiceScheduleModel().check_cron_format('hour', 0, 23, text)
+    except ConfigException:
+        return False
+    return True
+
+def cron_config_check_minutes(text: str) -> bool:
+    try:
+        ServiceScheduleModel().check_cron_format('minute', 0, 59, text)
+    except ConfigException:
+        return False
+    return True
