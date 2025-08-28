c335410ee416de4bc63d4380780f4d4f4f36f694
Scott DeWitt <sdewitt@newrelic.com>
2025-08-28 10:50:42 -0400

    chore: change version to 2.7.1 instead of 2.8.0

--- src/newrelic_logging/__init__.py
+++ src/newrelic_logging/__init__.py
@@ -3,7 +3,7 @@ from enum import Enum
 
 # Integration definitions
 
-VERSION         = "2.8.0"
+VERSION         = "2.7.1"
 NAME            = "salesforce-exporter"
 PROVIDER        = "newrelic-labs"
 COLLECTOR_NAME  = "newrelic-salesforce-exporter"
